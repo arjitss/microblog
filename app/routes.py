6d4b1586d7bfc21f6f4fc32e29c3c55be7945c84
Arjit <arjit.sharma@gmail.com>
2018-12-29 17:40:02 +0000

    adding database details

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+from flask import render_template, flash, redirect
 
 @app.route('/')
 @app.route('/index')
@@ -15,5 +17,13 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }	
 	]
-	return render_template('index.html', title = 'Home', user1 = user, posts = posts )
+	return render_template('index.html', title = 'Home', user = user, posts = posts )
 	#return " <H1> Hello " + user['username'] + " !! </H1>"
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+		return redirect('/index')
+	return render_template('login.html', title='Sign In', form=form)
